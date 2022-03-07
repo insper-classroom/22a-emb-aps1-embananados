f9248ce579b7b5651b5560b4400c762af55e32af
Lihsayuri <liviasm1@al.insper.edu.br>
2022-03-07 18:53:29 -0300

    Fazer o botão pausar a música

--- firmware/src/main.c
+++ firmware/src/main.c
@@ -63,12 +63,21 @@
 #define BUZ_IDX      30
 #define BUZ_IDX_MASK (1 << BUZ_IDX)
 
+volatile char but_flag; 
+volatile int tocando;
+
 typedef struct{
 	int tempo;
 	int wholenote;
 	int notes;
-	int melody[];
+	int *melody;
 } music;
+
+void but_callback(void)
+{
+	but_flag = 1;
+	
+}
 
 void tone(int freq, int time){
 	// a freq tá em segundos e time em ms
@@ -84,38 +93,108 @@ void tone(int freq, int time){
 }
 
 void play_music(music *musica){
-	for (int thisNote = 0; thisNote < ((*musica).notes)*2; thisNote = thisNote + 2) {
-		// calculates the duration of each note
-		int divider = (*musica).melody[thisNote + 1];
-		
-		int noteDuration = ((*musica).wholenote) / abs(divider);
-		if (divider < 0) {
-			noteDuration *= 1.5; // increases the duration in half for dotted notes
-		}
-		
-		if ((*musica).melody[thisNote] == 0){
-			// Wait for the specief duration before playing the next note.
-			delay_ms(noteDuration);
-		}
-		else{
-			// we only play the note for 90% of the duration, leaving 10% as a pause
-			tone((*musica).melody[thisNote], noteDuration * 0.9);
+	if (tocando){
+		for (int thisNote = 0; thisNote < ((*musica).notes)*2; thisNote = thisNote + 2) {
+			if (!pio_get(BUT1_PIO, PIO_INPUT, BUT1_PIO_IDX_MASK) && tocando == 1){
+				tocando = 0;
+				delay_ms(200);
+				break;
+			}
+			// calculates the duration of each note
+			int divider = (*musica).melody[thisNote + 1];
 			
-			// Wait for the specief duration before playing the next note.
-			delay_ms(noteDuration);
+			int noteDuration = ((*musica).wholenote) / abs(divider);
+			if (divider < 0) {
+				noteDuration *= 1.5; // increases the duration in half for dotted notes
+			}
+			
+			if ((*musica).melody[thisNote] == 0){
+				// Wait for the specief duration before playing the next note.
+				delay_ms(noteDuration);
+			}
+			else{
+				// we only play the note for 90% of the duration, leaving 10% as a pause
+				tone((*musica).melody[thisNote], noteDuration * 0.9);
+				
+				// Wait for the specief duration before playing the next note.
+				delay_ms(10);
+			}
+			
 		}
 		
 	}
 	
 }
 
-
-void io_init(void){
+void io_init(void)
+{
 	pmc_enable_periph_clk(BUZ_PIO_ID);
 	pio_configure(BUZ_PIO, PIO_OUTPUT_0, BUZ_IDX_MASK, PIO_DEFAULT);
 
-}
+	// Configura led
+	pmc_enable_periph_clk(LED2_PIO_ID);
+	pio_configure(LED2_PIO, PIO_OUTPUT_0, LED2_PIO_IDX_MASK, PIO_DEFAULT);
 
+	// Inicializa clock do periférico PIO responsavel pelo botao
+	pmc_enable_periph_clk(BUT1_PIO_ID);
+	pmc_enable_periph_clk(BUT2_PIO_ID);
+	pmc_enable_periph_clk(BUT3_PIO_ID);
+
+	// Configura PIO para lidar com o pino do botão como entrada
+	// com pull-up
+	//pio_configure(BUT_PIO, PIO_INPUT, BUT_IDX_MASK, PIO_PULLUP);
+	pio_configure(BUT1_PIO, PIO_INPUT, BUT1_PIO_IDX_MASK, PIO_PULLUP | PIO_DEBOUNCE);
+	pio_set_debounce_filter(BUT1_PIO, BUT1_PIO_IDX_MASK, 60);
+
+	pio_configure(BUT2_PIO, PIO_INPUT, BUT2_PIO_IDX_MASK, PIO_PULLUP | PIO_DEBOUNCE);
+	pio_set_debounce_filter(BUT2_PIO, BUT2_PIO_IDX_MASK, 60);
+	
+	pio_configure(BUT3_PIO, PIO_INPUT, BUT3_PIO_IDX_MASK, PIO_PULLUP | PIO_DEBOUNCE);
+	pio_set_debounce_filter(BUT3_PIO, BUT3_PIO_IDX_MASK, 60);
+
+	// Configura interrupção no pino referente ao botao e associa
+	// função de callback caso uma interrupção for gerada
+	// a função de callback é a: but_callback()
+	pio_handler_set(BUT1_PIO,
+	BUT1_PIO_ID,
+	BUT1_PIO_IDX_MASK,
+	PIO_IT_EDGE,
+	but_callback);
+
+	pio_handler_set(BUT2_PIO,
+	BUT2_PIO_ID,
+	BUT2_PIO_IDX_MASK,
+	PIO_IT_EDGE,
+	but_callback);
+	
+	pio_handler_set(BUT3_PIO,
+	BUT3_PIO_ID,
+	BUT3_PIO_IDX_MASK,
+	PIO_IT_EDGE,
+	but_callback);
+
+	//PIO_IT_RISE_EDGE, PIO_IT_FALL_EDGE
+	// Ativa interrupção e limpa primeira IRQ gerada na ativacao
+	pio_enable_interrupt(BUT1_PIO, BUT1_PIO_IDX_MASK);
+	pio_get_interrupt_status(BUT1_PIO);
+
+	pio_enable_interrupt(BUT2_PIO, BUT2_PIO_IDX_MASK);
+	pio_get_interrupt_status(BUT2_PIO);
+	
+	pio_enable_interrupt(BUT3_PIO, BUT3_PIO_IDX_MASK);
+	pio_get_interrupt_status(BUT3_PIO);
+	
+	// Configura NVIC para receber interrupcoes do PIO do botao
+	// com prioridade 4 (quanto mais próximo de 0 maior)
+	NVIC_EnableIRQ(BUT1_PIO_ID);
+	NVIC_SetPriority(BUT1_PIO_ID, 4); // Prioridade 4
+	
+	NVIC_EnableIRQ(BUT2_PIO_ID);
+	NVIC_SetPriority(BUT2_PIO_ID, 4); // Prioridade 4
+	
+	NVIC_EnableIRQ(BUT3_PIO_ID);
+	NVIC_SetPriority(BUT3_PIO_ID, 4); // Prioridade 4
+}
 
 int main (void)
 {
@@ -138,51 +217,30 @@ int main (void)
 	int notes_starwars = sizeof(melody_starwars) / sizeof(melody_starwars[0]) / 2;
 	int wholenote_starwars = (6000 * 4)/tempo_starwars; // está em ms
 
-	//music *starwars;
-	//music *nokia;
-	//
-	//music starwarsProperties;
-	//starwars = &starwarsProperties;
-	//
-	//music nokiaProperties;
-	//nokia = &nokiaProperties;
-	//
-	//(*starwars).tempo = 108;
-	//(*starwars).melody[] = melody_starwars;
-	//(*starwars).wholenote = (6000 * 4)/((*starwars).tempo);
-	//(*starwars).notes = sizeof(melody_starwars) / sizeof(melody_starwars[0]) / 2;
-
-
+	music starwars;
+	music nokia;
 	
+	
+	starwars.tempo = 108;
+	starwars.melody = &melody_starwars[0];
+	starwars.wholenote = (6000 * 4)/(starwars.tempo);
+	starwars.notes = sizeof(melody_starwars) / sizeof(melody_starwars[0]) / 2;
+
+
+	nokia.tempo = 180;
+	nokia.melody = &melody_nokia[0];
+	nokia.wholenote = (6000 * 4)/(nokia.tempo);
+	nokia.notes = sizeof(melody_nokia) / 4 / 2;
+	
+	tocando = 0;
   /* Insert application code here, after the board has been initialized. */
-	while(1){
-		//play_music(starwars);
-		for (int thisNote = 0; thisNote < notes_starwars*2; thisNote = thisNote + 2) {
-			// calculates the duration of each note
-			int divider = melody_starwars[thisNote + 1];
-			int noteDuration = (wholenote_starwars) / abs(divider);
-			if (divider < 0) {
-				noteDuration *= 1.5; // increases the duration in half for dotted notes
-			}
+	while(1){	
+		if (!pio_get(BUT1_PIO, PIO_INPUT, BUT1_PIO_IDX_MASK) && tocando == 0){
+			tocando = 1;
+			delay_ms(200);
+			play_music(&starwars);
+		} 
 				
-			if (melody_starwars[thisNote] == 0){
-				// Wait for the specief duration before playing the next note.
-				delay_ms(noteDuration);
-			}
-			else{
-				// we only play the note for 90% of the duration, leaving 10% as a pause
-				tone(melody_starwars[thisNote], noteDuration * 0.9);
-					
-
-				// Wait for the specief duration before playing the next note.
-				delay_ms(noteDuration);
-			}
-			
-		}
-		
-		//play_music(starwars);
-
-		
 	}
-
+		
 }
